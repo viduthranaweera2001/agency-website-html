be2d563712ced9e52746ccfb3084b677158fee18
udhantha20010630 <150140440+Udhantha30@users.noreply.github.com>
2024-08-04 20:02:23 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -12,3 +12,18 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+document.querySelector('.image-container').addEventListener('click', function() {
+  document.getElementById('fileInput').click();
+});
+
+document.getElementById('fileInput').addEventListener('change', function(event) {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      document.getElementById('uploadedAvatar').src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+});
